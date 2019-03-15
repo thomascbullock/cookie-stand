04460f28282d590c5f61111582afb3808696dd4e
THOM BULLOCK <t@thomascbullock.com>
2019-03-15 14:13:32 -0700

    added form and wired to table

--- js/app.js
+++ js/app.js
@@ -4,54 +4,16 @@ var hours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4p
 //I'm unsure about this hourly totals piece
 //Other approach I considered: building the html without totals and then crawling the table columns and summing them
 //That seemed wasteful as I did have all the information in memory once already
-//Con: an array 0s to add to just looks silly. Pro: otherwise I would have had to code special 'push' logic for the first time through the array. 
+//Con: an array 0s to add to just looks silly. Pro: otherwise I would have had to code special 'push' logic for the first time through the array.
 //Any feedback appreciated.
 
 var hourlyTotals = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 var grandTotal = 0;
 
-var pikeInfo = {
-  locationName: 'pike',
-  displayName: '1st and Pike',
-  minCustomers: 23,
-  maxCustomers: 65,
-  averageCookiesPerSale: 6.3
-};
-
-var seaTacInfo = {
-  locationName: 'seaTac',
-  displayName: 'SeaTac Airport',
-  minCustomers: 3,
-  maxCustomers: 23,
-  averageCookiesPerSale: 1.2
-};
-
-var seattleCenterInfo = {
-  locationName: 'seattleCenter',
-  displayName: 'Seattle Center',
-  minCustomers: 11,
-  maxCustomers: 38,
-  averageCookiesPerSale: 3.7
-};
-
-var capitolHillInfo = {
-  locationName: 'capitolHill',
-  displayName: 'Capitol Hill',
-  minCustomers: 20,
-  maxCustomers: 38,
-  averageCookiesPerSale: 2.3
-};
-
-var alkiInfo = {
-  locationName: 'alki',
-  displayName: 'Alki',
-  minCustomers: 2,
-  maxCustomers: 16,
-  averageCookiesPerSale: 4.6};
-
-
+var form = document.getElementById('sales-form');
 
 var tableBody = document.getElementById('sales-table-body');
+var table = document.getElementById('sales-table');
 
 //utility functions
 
@@ -62,21 +24,34 @@ function addTableRow(inRow){
 }
 
 function makeHeaderOrFooterRow(inData){
+  //headers and footers share a lot of code so making one function
+  //for the sake of variable names both types are referred to as a header
+  var type = inData.type;
   var firstCol = inData.firstCol;
   var arr = inData.arr;
   var lastCol = inData.lastCol;
-  var table = document.getElementById('sales-table');
+
+  var element;
+  switch(type) {
+  case 'header':
+    element = 'thead';
+    table.deleteTHead();
+    break;
+  case 'footer':
+    element = 'tfoot';
+    table.deleteTFoot();
+    break;
+  }
   //start the header with an empty cell
-  var headerRow = document.createElement('thead');
+  var headerRow = document.createElement(element);
   var headerData = `<td>${firstCol}</td>`;
   for (var headerCounter = 0; headerCounter < arr.length; headerCounter++) {
     headerData = `${headerData}<td>${arr[headerCounter]}</td>`;
   }
   //the totals get appended to the array at the end
   //therefore the header is short one column
-  //forcing a lastColumn if one is passed in
-  if (lastCol) {
-    headerData = `${headerData}<td>${inData.lastCol}</td>`;
+  if (type === 'header') {
+    headerData = `${headerData}<td>${lastCol}</td>`;
   }
 
   headerRow.innerHTML = headerData;
@@ -87,7 +62,6 @@ function makeHeaderOrFooterRow(inData){
 
 function Store(storeInfo) {
   this.locationName = storeInfo.locationName;
-  this.displayName = storeInfo.displayName;
   this.minCustomers = storeInfo.minCustomers;
   this.maxCustomers = storeInfo.maxCustomers;
   this.averageCookiesPerSale = storeInfo.averageCookiesPerSale;
@@ -104,8 +78,8 @@ function Store(storeInfo) {
 
   this.makeTableRow = function(){
     var calculatedDailyCookieTotal = 0;
-    //start table row with display name
-    this.calculatedCookiesTableRow = `<td>${this.displayName}</td>`;
+    //start table row with location name
+    this.calculatedCookiesTableRow = `<td>${this.locationName}</td>`;
     //now loop through the hours and calculate cookies per hour
     for (var hoursCounter = 0; hoursCounter < hours.length; hoursCounter++) {
       var calculatedCookies = this.getCookiesPerHour(this.getCustomersPerHour());
@@ -125,25 +99,26 @@ function Store(storeInfo) {
 
 //begin page logic here
 
-//make the header
-makeHeaderOrFooterRow({firstCol: '', lastCol: 'Daily Location Total', arr: hours});
+function formData(event){
+  event.preventDefault();
+  makeHeaderOrFooterRow({type: 'header', firstCol: '', lastCol: 'Daily Location Total', arr: hours});
+  var storeInfo = {
+    locationName: event.target.location_name.value,
+    minCustomers: event.target.min_cust.value,
+    maxCustomers: event.target.max_cust.value,
+    averageCookiesPerSale: event.target.average_cookies_per_sale.value
+  };
+  var store = new Store(storeInfo);
+  store.renderOutput();
+  //remove previous grand total if one exists (it won't the first time)
+  if (hourlyTotals.length > hours.length) {
+    hourlyTotals.pop();
+  }
+  //add grand total
+  hourlyTotals.push(grandTotal);
+  makeHeaderOrFooterRow({type: 'footer', firstCol: 'Totals', arr: hourlyTotals});
+  form.reset();
+}
 
-//create and add store rows
-var pike = new Store(pikeInfo);
-pike.renderOutput();
-var seaTac = new Store(seaTacInfo);
-seaTac.renderOutput();
-var seattleCenter = new Store(seattleCenterInfo);
-seattleCenter.renderOutput();
-var capitolHill = new Store(capitolHillInfo);
-capitolHill.renderOutput();
-var alki = new Store(alkiInfo);
-alki.renderOutput();
+form.addEventListener('submit',formData);
 
-//Add grandTotal to hourly totals array
-hourlyTotals.push(grandTotal);
-
-//make the footer
-makeHeaderOrFooterRow({firstCol: 'Totals', arr: hourlyTotals});
-
-//profit
